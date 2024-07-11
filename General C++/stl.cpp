d12bb3b254440c5bd871b44343081b36f8cd640f
kunal89204 <kunalkhandelwal108@gmail.com>
2024-07-11 21:54:56 +0530

    some stl stuff

---
+++ General C++/stl.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+void print(){
+    cout<<"kunal";
+}
+
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){
+
+    // print();
+    // int ans = sum(1,7);
+    // cout<<ans;
+
+    // pair<int , pair<int, int>> p = {1,{4,7}};
+    // cout<<p.second.second;
+
+    // pair<int, int>arr[] = {{1, 2}, {3, 4}, {5, 6}};
+    // return 0;
+
+
+    // Vectors
+
+    vector<int> v;
+
+    v.push_back(1);
+    v.emplace_back(2);
+
+    // vector of pair
+
+    vector<pair<int, int>>vec;
+
+}
