f9c87e02335ca0f6303ad91ee718da916b76aa78
kunal89204 <kunalkhandelwal108@gmail.com>
2024-11-29 18:00:20 +0530

    some changes

---
+++ windows.cpp
@@ -0,0 +1,6 @@
+#include<windows.h>
+
+int WINAPI WinMain(HINSTANCE hint, HINSTANCE hprev, LPSTR agr, int ncmd){
+    MessageBox(NULL, "Welcome to kunal", "first gui app", MB_OKCANCEL);
+    return 0;
+}
